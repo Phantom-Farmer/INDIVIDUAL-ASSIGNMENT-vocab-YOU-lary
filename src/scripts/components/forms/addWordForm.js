6f0be7ad35c4429304f1ff09595fc6d425ef49b1
Phantom-Farmer <joeljdahl@gmail.com>
2022-06-10 14:41:38 -0500

    on to events

--- src/scripts/components/forms/addWordForm.js
+++ src/scripts/components/forms/addWordForm.js
@@ -1,22 +1,27 @@
+import clearDom from '../../helpers/clearDom';
 import renderToDom from '../../helpers/renderToDom';
 
-const addWordForm = () => {
-  let domString = '';
-  domString = `
+const addWordForm = (obj = {}) => {
+  clearDom();
+  const domString = `
 <form>
   <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="type" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <label for="title-term" class="form-label">Enter Word</label>
+    <input type="type" class="form-control" id="title-term" aria-describedby="term">
+    <div id="emailHelp" class="form-text">Taking your tech termonology to the MAX!!!</div>
   </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <label for="define" class="form-label">Definition</label>
+    <input type="type" class="form-control" id="define">
   </div>
-  <div class="form-group">
-    <div class="form-group" id="select-category"></div>
-    </div>
-    </div>
+  <label for="select-tech">Tech Category</label>
+  <select class="form-select" aria-label="Select Tech Category" id="select-tech">
+    <option selected>${obj.tech || 'Select Tech Category'}</option>
+    <option value="HTML">HTML</option>
+    <option value="JavaScript">JavaScript</option>
+    <option value="Python">Python</option>
+    <option value="CSS">CSS</option>
+  </select>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>`;
   renderToDom('#form-container', domString);
