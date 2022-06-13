f890cbea7bedf9d2649c85f96d7b6de9937d46a9
Phantom-Farmer <joeljdahl@gmail.com>
2022-06-12 23:11:28 -0500

    boom-shaka-laka

--- src/scripts/components/navBar.js
+++ src/scripts/components/navBar.js
@@ -1,9 +1,11 @@
+import logo from '../assets/logo.jpg';
 import renderToDom from '../helpers/renderToDom';
 
 const navBar = () => {
   const domString = `
   <nav class="navbar navbar-light bg-light" id="barNav">
   <div class="container-fluid">
+    <img src="${logo}" alt="logo" >
     <span class="navbar-brand mb-0 h1">WORDMAX X-TREME!!!</span>
     <button type="button" class="newWordBtn btn btn-info" id="addWord">NEW WORD</button>
     <div class="logoutBtn" id="logoutBTN"></div>
--- src/scripts/helpers/clearDom.js
+++ src/scripts/helpers/clearDom.js
@@ -1,5 +1,7 @@
 const clearDom = () => {
   document.querySelector('#login-form-container').innerHTML = '';
+  document.querySelector('#form-container').innerHTML = '';
+  document.querySelector('#words-container').innerHTML = '';
   document.querySelector('#filters').innerHTML = '';
 };
 
